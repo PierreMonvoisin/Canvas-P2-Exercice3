06a4c97d835367649c26501b1dabcd2154ae42fb
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-11-25 22:47:44 +0100

    Exercice presque terminé, besoin d'ajout de rounded corner sur les bras et jambes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,74 @@ $(function(){
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
 
-  // Triangle
+  // Tête
   context.beginPath();
-  context.moveTo(200, 300);
-  context.lineTo(170, 200);
-  context.lineTo(230, 200);
+  context.fillStyle = '#976F0F';
+  context.arc(200, 65, 55, 0, 2*Math.PI);
   context.closePath();
-  context.fillStyle = "#AA6522";
   context.fill();
+    // Sourcil gauche
+    context.beginPath();
+    context.moveTo(170, 37);
+    context.strokeStyle = "#FFFFFF";
+    context.quadraticCurveTo(180, 27, 190, 37);
+    context.stroke();
+    // Oeil gauche
+    context.beginPath();
+    context.fillStyle = '#FFFFFF';
+    context.arc(180, 55, 10, 0, 2*Math.PI);
+    context.closePath();
+    context.fill();
 
-  // Courbe
-  context.beginPath();
-  context.lineWidth = '1';
-  context.strokeStyle = '#8A0908';
-  context.fillStyle = '#8A0908'
-  context.moveTo(170, 200);
-  context.bezierCurveTo(160, 140, 240, 140, 230, 200);
-  context.stroke();
-  context.fill();
+    // Sourcil droit
+    context.beginPath();
+    context.moveTo(210, 37);
+    context.strokeStyle = "#FFFFFF";
+    context.quadraticCurveTo(220, 27, 230, 37);
+    context.stroke();
+    // Oeil droit
+    context.beginPath();
+    context.fillStyle = '#FFFFFF';
+    context.arc(220, 55, 10, 0, 2*Math.PI);
+    context.closePath();
+    context.fill();
+
+    // Lèvre haut
+    context.beginPath();
+    context.moveTo(175, 85);
+    context.lineWidth = '2'
+    context.strokeStyle = "#DE2204";
+    context.quadraticCurveTo(200, 97, 225, 85);
+    context.stroke();
+    // Lèvre bas
+    context.beginPath();
+    context.moveTo(175, 86);
+    context.lineWidth = '2'
+    context.strokeStyle = "#DE2204";
+    context.quadraticCurveTo(200, 110, 225, 86);
+    context.stroke();
+  // Corps
+  context.fillStyle = '#976F0F';
+  context.fillRect(150, 100, 100, 200);
+    // Boutton haut
+    context.beginPath();
+    context.fillStyle = '#7A2976';
+    context.arc(200, 200, 10, 0, 2*Math.PI);
+    context.closePath();
+    context.fill();
+    // Boutton bas
+    context.beginPath();
+    context.fillStyle = '#7A2976';
+    context.arc(200, 160, 10, 0, 2*Math.PI);
+    context.closePath();
+    context.fill();
+  // Bras
+  context.fillStyle = '#976F0F';
+  context.fillRect(40, 100, 320, 40);
+  // Pied gauche
+  context.fillStyle = '#976F0F';
+  context.fillRect(150, 300, 40, 50);
+  // Pied droit
+  context.fillStyle = '#976F0F';
+  context.fillRect(210, 300, 40, 50);
 });
